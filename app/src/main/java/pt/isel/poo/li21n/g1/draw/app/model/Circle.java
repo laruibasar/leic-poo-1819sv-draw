47d017a44bccce09e8d24004593af883a04b021c
glourenco <ghmlourenco@gmail.com>
2019-05-02 19:39:03 +0100

    Figure, Point + Circle

---
+++ app/src/main/java/pt/isel/poo/li21n/g1/draw/app/model/Circle.java
@@ -0,0 +1,39 @@
+package pt.isel.poo.li21n.g1.draw.app.model;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Circle extends Figure {
+
+    public char letter;
+    private int radius;
+
+    public Circle(int x, int y){
+
+    }
+
+    public Circle(){
+
+    }
+
+    protected char getLetter(){
+        return this.letter;
+    }
+
+    public void load(Scanner in){
+
+    }
+
+    public void save(PrintWriter out){
+
+    }
+
+    public void setEnd(int x, int y){
+
+    }
+
+    public int getRadius(){
+        return this.radius;
+    }
+
+}
